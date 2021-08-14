ceb14d0ac4aae6dc146dab87b9ea3856bb31c44d
Paul OHL <pohl@student.42.fr>
2021-08-14 17:17:22 +0200

    Started experimenting with ascii

---
+++ incs/useful_structs.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   useful_structs.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pohl <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/14 14:36:30 by pohl              #+#    #+#             */
+/*   Updated: 2021/08/14 16:32:40 by pohl             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef USEFUL_STRUCTS_H
+# define USEFUL_STRUCTS_H
+
+typedef struct	s_ivector2
+{
+	int	x;
+	int	y;
+}	t_ivector2;
+
+typedef struct	s_complex
+{
+	double	r;
+	double	i;
+}	t_complex;
+
+#endif
