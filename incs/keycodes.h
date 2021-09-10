76d45041d9578efa65ddb5d0f93e7f48e2d76d77
popohl <pohl@student.42.fr>
2021-09-10 19:00:31 +0200

    Started working on keycodes

---
+++ incs/keycodes.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycodes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulohl <pohl@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 15:44:24 by paulohl           #+#    #+#             */
+/*   Updated: 2021/09/07 15:49:50 by paulohl          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODES_H
+# define KEYCODES_H
+
+# define KC_Q 12
+# define KC_W 13
+# define KC_E 14
+# define KC_R 15
+# define KC_T 17
+# define KC_Y 16
+# define KC_U 32
+# define KC_I 34
+# define KC_O 31
+# define KC_P 35
+# define KC_A 0
+# define KC_S 1
+# define KC_D 2
+# define KC_F 3
+# define KC_G 5
+# define KC_H 4
+# define KC_J 38
+# define KC_K 40
+# define KC_L 37
+# define KC_Z 6
+# define KC_X 7
+# define KC_C 8
+# define KC_V 9
+# define KC_B 11
+# define KC_N 45
+# define KC_M 46
+# define KC_LEFT_ARR 123
+# define KC_RIGHT_ARR 124
+# define KC_UP_ARR 126
+# define KC_DOWN_ARR 125
+# define KC_LEFT_SHIFT 257
+# define KC_RIGHT_SHIFT 258
+# define KC_LEFT_CTRL 256
+# define KC_RIGHT_CTRL 269
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pohl <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 11:32:23 by pohl              #+#    #+#             */
-/*   Updated: 2021/09/07 13:16:06 by paulohl          ###   ########.fr       */
+/*   Updated: 2021/09/07 15:52:40 by paulohl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 #include "config_initialisation.h"
 #include "checkers.h"
 #include "draw.h"
+#include "keycodes.h"
+
+int	key_press(int keycode, void *param)
+{
+	if (keycode == KC_H)
+	{
+		printf("hi!\n");
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -28,7 +38,7 @@ int	main(int argc, char **argv)
 		return (1);
 	draw_fractal(&config);
 	/* mlx_string_put(config.mlx.mlx_ptr, config.mlx.win_ptr, 50, 50, 0x00ffffff, "Hi there!"); */
-	/* mlx_hook(config.mlx.win_ptr, int x_event, int x_mask, int (*funct)(), &config); */
+	mlx_hook(config.mlx.win_ptr, 2, 1L<<0, key_press, &config);
 	mlx_loop(config.mlx.mlx_ptr);
 	return (0);
 }
